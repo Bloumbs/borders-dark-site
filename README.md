1e2b641f57a92e9fdc508c5f22f00787b867007b
Bloumbs <baysicsss@gmail.com>
2019-06-19 11:25:12 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# borders-ios-dark
-Just a super simple gh-pages site that redirects to my VSCode theme Borders Dark
+<img src="https://raw.githubusercontent.com/Bloumbs/borders-ios-badge/master/images/ios-badge.gif" width="900">
+
+# borders-ios-badge
+
+Just a super simple gh-pages site that redirects to my VSCode theme [Borders Dark](https://marketplace.visualstudio.com/items?itemName=bloumbs.borders-dark)
---
+++ main.js
@@ -0,0 +1,48 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const url = "https://marketplace.visualstudio.com/items?itemName=bloumbs.borders-dark"
+
+let intDown = 0
+
+window.onload = function () {
+   scrape();
+};
+
+function scrape() {
+   axios.get(url).then((response) => {
+      console.log("SCRAPE STARTED....")
+      const $ = cheerio.load(response.data)
+
+      let downloads = $('.ux-item-rating .downloads-text').html($.downloadsText)
+      let downSplit = String(downloads.split(" ")[1]).replace(/,/g, "")
+      intDown = parseInt(downSplit)
+      
+      main();
+   })
+}
+
+function main() {
+   var nf = new Intl.NumberFormat()
+   var downloadCount = intDown
+
+   console.log("BEFORE SCRAPE:", intDown)
+   const badge = document.getElementById("badge");
+   var i = 1;
+
+   function count() {
+      setTimeout(function () {
+         if (i < downloadCount + 1) {
+            badge.innerText = nf.format(i)
+            i++
+            count()
+         } else {
+            console.log("AFTER SCRAPE:", intDown)
+            setTimeout(function () {
+               badge.style.textAlign = "center"
+               badge.innerText = "1.7.0"
+            }, 1000)
+         }
+      }, 0)
+   }
+   count()
+}
