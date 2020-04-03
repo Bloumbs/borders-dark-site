ecdcc0d5f40060a6cfb098a6f9e83b4b88ea944a
Bloumbs <baysicsss@gmail.com>
2020-04-03 09:20:11 -0500

    updated borders-dark landing page site
    more work will be done to it later on

--- main.js
+++ main.js
@@ -10,11 +10,11 @@ window.onload = function () {
 
 function scrape() {
     axios.get(url).then((response) => {
-        console.log("Getting Borders Dark info...");
+        console.log("Getting Borders Dark v1.7.0 info...");
         const $ = cheerio.load(response.data);
 
         let installs = $('.ux-item-rating .installs-text').html($.installsText);
-        let install_num = String(installs.split(" ")[1]);
+        let install_num = String(installs.split(" ")[1]).replace(',', '');
         int_installs = parseInt(install_num);
 
         main();
@@ -26,21 +26,22 @@ function main() {
     let install_count = int_installs;
 
     console.log("Installs:", int_installs);
-    const badge = document.getElementById("badge");
+    const badge = document.getElementById("version");
+    //badge.style.textAlign = "left";
     let i = 1;
 
     function count() {
         setTimeout(function () {
             if (i < install_count + 1) {
                 badge.innerText = nf.format(i);
-                i++;
+                i += 3;
                 count();
             } else {
+                badge.innerText = `${nf.format(i)} Installs`;
                 setTimeout(function () {
-                    badge.style.textAlign = "center";
-                    badge.innerText = "1.7.0";
-                    console.log("Version: 1.7.0");
-                }, 1000);
+                    //badge.style.textAlign = "center";
+                    badge.innerText = "v1.7.0";
+                }, 1500);
             }
         }, 0);
     }
